b91eb1fd22f8bde568fab8e2f2ed704f47d8ac8d
thanhcvph24894 <thanhcvph24894@fpt.edu.vn>
2025-04-06 16:08:08 +0700

    menu

---
+++ frontend/App.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+
+const App = () => {
+ // viết code bằng js ở đây
+ console.log("Chạy component APP");
+  return (
+   <View>
+     <Text>Ứng dụng mở đầu</Text>
+   </View>
+ )
+}
+
+
+export default App
+
+
+const styles = StyleSheet.create({})
